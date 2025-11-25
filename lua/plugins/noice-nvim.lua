03501d49dbd587c11d955799ee04ca3aa880a6fd
Eht67 <logictechie@gmail.com>
2025-11-25 18:31:48 +0500

    add @macro recording display in noice, set bg color for notify which matches cobalt2 theme

--- lua/plugins/noice-nvim.lua
+++ lua/plugins/noice-nvim.lua
@@ -2,14 +2,26 @@ return {
 	{
 		"folke/noice.nvim",
 		event = "VeryLazy",
-		opts = {},
+		opts = {
+			routes = {
+				{
+					view = "notify",
+					filter = { event = "msg_showmode" },
+				},
+			},
+		},
 		dependencies = {
-			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
 			"MunifTanjim/nui.nvim",
-			-- OPTIONAL:
-			--   `nvim-notify` is only needed, if you want to use the notification view.
-			--   If not available, we use `mini` as the fallback
 			"rcarriga/nvim-notify",
 		},
+		config = function(_, opts)
+			-- notify setup
+			require("notify").setup({
+				background_colour = "#193549",
+			})
+
+			-- noice setup
+			require("noice").setup(opts)
+		end,
 	},
 }
