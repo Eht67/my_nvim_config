5b2bcdf5ad5e81d350581f467408adb6c2d81b91
Eht67 <logictechie@gmail.com>
2025-11-25 14:12:42 +0500

    add keybinding to rename things using lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -47,6 +47,7 @@ return {
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set({ "n", "v" }, "<leader>rn", vim.lsp.buf.rename, {})
 			vim.diagnostic.config({
 				virtual_text = true,
 			})
