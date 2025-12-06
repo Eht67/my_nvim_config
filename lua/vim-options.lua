1748e0c272af553f170d1247173c2df1445f537c
Eht67 <logictechie@gmail.com>
2025-12-06 11:01:49 +0500

    integrate nvim better into tmux navigation

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,12 +6,12 @@ vim.cmd("set shiftwidth=2")
 
 -- highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
-  callback = function()
-    vim.highlight.on_yank({
-      higroup = "HighlightUndo",
-      timeout = 300,
-    })
-  end,
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = "HighlightUndo",
+			timeout = 300,
+		})
+	end,
 })
 
 -- enable relative line numbers with absolute line number on current line
@@ -24,9 +24,17 @@ vim.g.mapleader = " "
 -- change the eob tilde with dots
 vim.opt.fillchars:append({ eob = "." })
 
--- set current working directory as vim root 
+-- set current working directory as vim root
 vim.g.root_spec = { "cwd" }
 
 -- keybinding to open terminal
 vim.keymap.set("n", "T", ":botright split | :resize 20 | :terminal<CR>", {})
 
+-- disable highlighting
+vim.keymap.set("n", "<leader>h", ":noh<CR>", {})
+
+-- navigation speed keymaps
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
