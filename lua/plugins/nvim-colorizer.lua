8c5519fd691f0203dafe0c5c6433a18bddcbab58
Eht67 <logictechie@gmail.com>
2025-12-24 20:25:38 +0500

    add indent lines plugin

--- lua/plugins/nvim-colorizer.lua
+++ lua/plugins/nvim-colorizer.lua
@@ -34,7 +34,7 @@ return {
 			-- Tailwind colors.  boolean|'normal'|'lsp'|'both'.  True sets to 'normal'
 			tailwind = true, -- Enable tailwind colors
 			tailwind_opts = { -- Options for highlighting tailwind names
-				update_names = false, -- When using tailwind = 'both', update tailwind names from LSP results.  See tailwind section
+				update_names = true, -- When using tailwind = 'both', update tailwind names from LSP results.  See tailwind section
 			},
 			-- parsers can contain values used in `user_default_options`
 			sass = { enable = true, parsers = { "css" } }, -- Enable sass colors
