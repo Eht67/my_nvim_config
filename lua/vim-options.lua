8a8f1f3aecf77da5aa75fa84869109828353f6ca
Eht67 <logictechie@gmail.com>
2025-12-25 20:44:18 +0500

    highlight cursor line, rearrange hjkl keymaps in more loical order

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -34,7 +34,10 @@ vim.keymap.set("n", "T", ":botright split | :resize 20 | :terminal<CR>", {})
 vim.keymap.set("n", "<leader>h", ":noh<CR>", {})
 
 -- navigation speed keymaps
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
+
+-- highlight cursor line
+vim.opt.cursorline = true
